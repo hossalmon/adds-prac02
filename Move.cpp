4348d134a753b65f94ba68126a63290028e8c526
Hossam Amin <a1705067@student.adelaide.edu.au>
2025-08-10 22:45:45 +0930

    fixed casing to fit with gradescope tests haha

--- Move.cpp
+++ Move.cpp
@@ -1,14 +1,14 @@
 #include "Move.h"
 #include <unordered_map>
 #include <string>
-#include <cctype>
 
 // NamedMove implementation
-NamedMove::NamedMove(const std::string& name, const std::unordered_set<std::string>& winsAgainst) :
-    name_(name), winsAgainst_(winsAgainst) {}
+NamedMove::NamedMove(const std::string& name,
+                     const std::unordered_set<std::string>& winsAgainst)
+    : name_(name), winsAgainst_(winsAgainst) {}
 
 std::string NamedMove::getName() const {
-    return name_;
+    return name_; // returns name of the move made
 }
 
 bool NamedMove::beats(const Move& other) const {
@@ -16,34 +16,25 @@ bool NamedMove::beats(const Move& other) const {
 }
 
 // MoveFactory implementation
-
-// Normalise to UPPERCASE for input case sensitivity
-static std::string norm(std::string s) {
-    for (char& c : s) c = static_cast<char>(std::toupper(static_cast<unsigned char>(c)));
-    return s;
-}
-
 Move* MoveFactory::fromString(const std::string& userInput) {
-    const std::string key= norm(userInput);
-
-    // each key lists who that move beats
+    // each value lists who that move beats
     static const std::unordered_map<std::string, std::unordered_set<std::string>> wins = {
-        // rock paper scissors
-        {"ROCK",    {"SCISSORS"}},
-        {"PAPER",   {"ROCK"}},
-        {"SCISSORS",    {"PAPER"}},
+        // Rock–Paper–Scissors rules
+        {"Rock",     {"Scissors"}},
+        {"Paper",    {"Rock"}},
+        {"Scissors", {"Paper"}},
 
-        // monkey, robot, ninja, zombie rules
-        {"MONKEY",  {"NINJA", "ROBOT"}},
-        {"ROBOT",   {"NINJA", "ZOMBIE"}},
-        {"PIRATE",  {"ROBOT", "MONKEY"}},
-        {"NINJA",   {"PIRATE", "ZOMBIE"}},
-        {"ZOMBIE",  {"PIRATE", "MONKEY"}},
+        // Monkey–Robot–Pirate–Ninja–Zombie rules
+        {"Monkey",   {"Ninja", "Robot"}},
+        {"Robot",    {"Ninja", "Zombie"}},
+        {"Pirate",   {"Robot", "Monkey"}},
+        {"Ninja",    {"Pirate", "Zombie"}},
+        {"Zombie",   {"Pirate", "Monkey"}},
     };
 
-    auto it = wins.find(key);
+    auto it = wins.find(userInput);
     if (it != wins.end()) {
-        return new NamedMove(key, it->second);
+        return new NamedMove(userInput, it->second); // caller (Referee) deletes
     }
-  
+    
 }
